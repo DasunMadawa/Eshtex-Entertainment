0caa450127005058aa7b2f122219a46b758fc464
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2024-12-01 13:21:08 +0530

    feat: cover component responsived

--- components/event/Cover.tsx
+++ components/event/Cover.tsx
@@ -12,13 +12,16 @@ export default function Cover() {
             {/* buy ticket button */}
             <div className='w-full h-full flex items-center justify-center'>
                 {/* <button>Buy Ticket</button> */}
-                <button className="button-89" role="button">
-                    <span className=' text-2xl font-bold'>Buy Tickets</span>
-                </button>
+                <div className='bg-black/10 p-5 rounded-lg backdrop-blur-sm'>
+                    <button className="button-89" role="button">
+                        <span className=' text-2xl font-bold'>Buy Tickets</span>
+                    </button>
+                </div>
+
             </div>
 
             {/* event title */}
-            <h1 className='absolute left-16 bottom-10 text-white text-8xl font-bold italic'>Mahoraga</h1>
+            <h1 className='absolute left-5 md:left-10 lg:left-16 bottom-3 md:bottom-6 lg:bottom-10 text-white text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold italic'>Mahoraga</h1>
         </div>
     )
 }
