6871bd40eedb93c2437d5fef762db51fe579f326
Dasun Madawa <dasunmadawa8864@gmail.com>
2025-07-04 20:43:05 +0530

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://manobhawa.lk', // 🔁 Change this to your custom domain
+  generateRobotsTxt: true,         // ✅ Generate robots.txt file
+//   sitemapSize: 7000,               // Optional, for large sites
+};
